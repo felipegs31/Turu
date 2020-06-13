669254e393fcad5b43a92b9fe36b6b7251e3c040
Matheus Gaya Scandiffio <matheus@nexmob.com.br>
2020-06-13 16:06:50 -0300

    fix

--- src/components/HeartBeat/HeartBeat.js
+++ src/components/HeartBeat/HeartBeat.js
@@ -1,35 +1,56 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   View, Text, SafeAreaView, StyleSheet
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import ProgressCircle from 'react-native-progress-circle'
+import LottieView from 'lottie-react-native'
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 const HeartBeat = (props) => {
-  const dispatch = useDispatch();
+  const [heartBeat, setHeartBeat] = useState(64)
+  const [percent, setPercent] = useState(0)
 
-  const heartBeat = useSelector(state => state.heartBeat.heartBeat)
+  useInterval(() => {
+    setHeartBeat(heartBeat + Math.floor(Math.random() * 8) - 4)
+    setPercent(Math.floor(100*heartBeat/160))
+  }, 2000);
 
   return (
     <View style={styles.container}>
       <ProgressCircle
-            percent={30}
-            radius={150}
-            borderWidth={8}
-            color="#3399FF"
-            shadowColor="#999"
-            bgColor="#000"
-        >
+        percent={percent}
+        radius={160}
+        borderWidth={8}
+        color="#3399FF"
+        shadowColor="#999"
+        bgColor="#000"
+      >
         <View style={styles.display}>
-          <Text style={{color: "#FFF", fontSize: 90}}>{heartBeat}</Text>
+          <Text style={{ color: "#FFF", fontSize: 90 }}>{heartBeat}</Text>
           <View style={styles.unityContainer}>
-            <Icon
-              name='md-heart'
-              size={50}
-              color="#FF0000"
-            />
-            <Text style={{color: "grey", fontSize: 30}}>bpm</Text>
+            <LottieView style={{ marginTop: -5, width: 80, height: 80 }} source={require('./heart.json')} autoPlay loop speed={0.5} />
+            <Text style={{ marginTop: -40, color: "grey", fontSize: 30 }}>bpm</Text>
           </View>
         </View>
       </ProgressCircle>
