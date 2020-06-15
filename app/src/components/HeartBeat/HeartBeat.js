06fc35b056c81e9b17bbc7b8bbf09019eb40695a
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2020-06-14 23:36:56 -0300

    remove ble

--- app/src/components/HeartBeat/HeartBeat.js
+++ app/src/components/HeartBeat/HeartBeat.js
@@ -7,6 +7,7 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import ProgressCircle from 'react-native-progress-circle'
 import LottieView from 'lottie-react-native'
 import Colors from '../../constants/Colors';
+import BleManager from 'react-native-ble-manager';
 
 function useInterval(callback, delay) {
   const savedCallback = useRef();
@@ -36,11 +37,52 @@ const HeartBeat = (props) => {
   const [heartBeat, setHeartBeat] = useState(64)
   const [percent, setPercent] = useState(0)
 
+  //for sake of demo
   useInterval(() => {
     const rand = randomInteger(-1,1);
     setHeartBeat(heartBeat + rand)
     setPercent(Math.floor(100*heartBeat/160))
   }, 2000);
+
+  // useEffect(() => {
+  //   bluetooth()
+  // }, [])
+
+  const bluetooth = () => {
+    const deviceUUID =
+      Platform.OS === 'ios'
+        ? 'A0AA0000-000A-00AA-AAAA-AA000AA00A00'
+        : 'A0:00:0A:00:A0:00'
+    BleManager.startDeviceScan(null, null, async (error, device) => {
+      if (error) {
+        console.error(error)
+        BleManager.cancelDeviceConnection(deviceUUID)
+      }
+      console.log(device)
+      if (device && device.localName === 'Device XX-00') {
+        const jsonDataServiceUUID = 'A0AA0000-000A-00AA-AAAA-AA000AA00A00'
+
+        const jsonFormattedCharacteristicUUID ='A0AA0000-000A-00AA-AAAA-AA000AA00A00'
+
+        BleManager.stopDeviceScan()
+        await BleManager.connectToDevice(deviceUUID)
+
+        await BleManager.discoverAllServicesAndCharacteristicsForDevice(deviceUUID)
+
+        const read = await BleManager.readCharacteristicForDevice(
+          deviceUUID,
+          jsonDataServiceUUID,
+          jsonFormattedCharacteristicUUID,
+        )
+        if (read.value) {
+          console.log(read)
+          const responseJson = Buffer.from(read.value, 'base64').toString()
+          console.log(responseJson)
+          setHeartBeat(responseJson);
+        }
+      }
+    })
+  }
 
   return (
     <View style={styles.container}>
